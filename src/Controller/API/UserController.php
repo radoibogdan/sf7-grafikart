01df3497fea8cdffdb5a5b981927f1358ec7ed1f
Bogdan Radoi <radoi.office@gmail.com>
2025-01-25 20:03:15 +0100

    Authentication API User via apiToken

---
+++ src/Controller/API/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\API;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+class UserController extends AbstractController
+{
+    #[Route('/api/me')]
+    #[IsGranted('ROLE_USER')]
+    public function me(): Response
+    {
+        dd($this->getUser());
+        return $this->json(['message' => 'Bonjour']);
+    }
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -40,6 +40,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private bool $isVerified = false;
 
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $apiToken = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -144,6 +147,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setVerified(bool $isVerified): static
     {
         $this->isVerified = $isVerified;
+
+        return $this;
+    }
+
+    public function getApiToken(): ?string
+    {
+        return $this->apiToken;
+    }
+
+    public function setApiToken(?string $apiToken): static
+    {
+        $this->apiToken = $apiToken;
 
         return $this;
     }
